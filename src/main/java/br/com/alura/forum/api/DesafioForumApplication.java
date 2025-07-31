bd885b1f8d5963f56ee02afa9e35e03d8f07639f
Anselmo Luiz <ansehelm2001@gmail.com>
2025-07-31 17:38:06 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/forum/api/DesafioForumApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.forum.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/forum/api/DesafioForumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.forum.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
